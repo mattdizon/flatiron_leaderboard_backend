6ab88dcb0d6ab25eff953ca4f7939dfdc3f99448
github username <matthewdizon94@protonmail.com>
2019-02-25 17:24:54 -0500

    ps validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: {case_sensitive: false}
     validates :email, uniqueness: {case_sensitive: false}
+    validates :email, uniqueness: {case_sensitive: false}
+     validates :password, :presence => true,
+                         :length => {:within => 6..40},
+                         :on => :create
     has_many :rejections
 
 

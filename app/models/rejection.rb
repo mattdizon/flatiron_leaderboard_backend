8ce047ff9a236cf2cfe7595ad8d1534b6e43be59
github username <matthewdizon94@protonmail.com>
2019-02-25 23:08:14 -0500

    fixed api

--- app/models/rejection.rb
+++ app/models/rejection.rb
@@ -1,3 +1,10 @@
 class Rejection < ApplicationRecord
   belongs_to :user
+  def as_json(options={})
+       super(
+             :include => {
+               :user => {:only => [:username]}
+             }
+       )
+     end
 end
